4899ab1b63972f86cb6af2b5752535f97d0f388b
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-11-27 22:26:46 +0300

    add cors

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const { errors } = require('celebrate');// обработчик ошибок celebrate
-const cors = require('cors');
-const { rateLimit } = require('express-rate-limit');
-const { DB_URL } = require('./config');
-const router = require('./routes');
-const errorHandler = require('./middlewares/errorHandler');
-const corsError = require('./middlewares/corsError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const { errors } = require("celebrate"); // обработчик ошибок celebrate
+const cors = require("cors");
+const { rateLimit } = require("express-rate-limit");
+const { DB_URL } = require("./config");
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
+// const corsError = require('./middlewares/corsError');
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
+
+const corseAllowedOrigins = [
+  "https://movies.balineseleaf.nomoredomainsrocks.ru",
+  "http://movies.balineseleaf.nomoredomainsrocks.ru",
+  "http://localhost:3000",
+  "http://localhost:3001",
+];
 
 // параметры порта
 const { PORT = 3000 } = process.env;
 // подключаемся к базе данных
-mongoose.connect(DB_URL, {
-  useNewUrlParser: true,
-}).then(() => console.log('Connected to mongodb'));
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to mongodb"));
 
 // Создаем приложение
 const app = express();
@@ -32,18 +41,25 @@ app.use(cors());
 
 app.use(helmet());
 
+app.use(express.json()); // обработка запросов json
+
 app.use(requestLogger); // подключаем логгер запросов
 
 // ограничиваем кол-во запросов
 app.use(limiter);
 
-app.use(express.json()); // обработка запросов json
-
 app.use(router);
 
 app.use(errorLogger); // подключаем логгер ошибок
 
-app.use(corsError); // cors
+// используем cors
+app.use(
+  cors({
+    origin: corseAllowedOrigins,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(errors()); // обработчик ошибок celebrate
 // errors() будет обрабатывать только ошибки, которые сгенерировал celebrate.
--- middlewares/corsError.js
+++ middlewares/corsError.js
@@ -1,30 +1,30 @@
-const allowedCors = [
-  "https://movies.balineseleaf.nomoredomainsrocks.ru",
-  "http://movies.balineseleaf.nomoredomainsrocks.ru",
-  "http://localhost:3000",
-  "http://localhost:3001",
-];
+// const allowedCors = [
+//   'https://movies.balineseleaf.nomoredomainsrocks.ru',
+//   'http://movies.balineseleaf.nomoredomainsrocks.ru',
+//   'http://localhost:3000',
+//   'http://localhost:3001',
+// ];
 
-module.exports = (req, res, next) => {
-  const { method } = req; // тип запроса (HTTP-метод)
-  const { origin } = req.headers; // источники запросов
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers["access-control-request-headers"];
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+// module.exports = (req, res, next) => {
+//   const { method } = req; // тип запроса (HTTP-метод)
+//   const { origin } = req.headers; // источники запросов
+//   // сохраняем список заголовков исходного запроса
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Credentials", true);
-  }
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === "OPTIONS") {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
 
-  return next();
-};
+//   return next();
+// };
