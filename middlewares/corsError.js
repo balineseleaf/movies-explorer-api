b777ff22c885dfc40b90149081b5b80c95aca291
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-11-27 13:08:13 +0300

    add my domain link

--- middlewares/corsError.js
+++ middlewares/corsError.js
@@ -1,25 +1,27 @@
-const allowedCors = [// мои домены добавить
-  'http://localhost:3000',
-  'http://localhost:3001',
+const allowedCors = [
+  "https://movies.balineseleaf.nomoredomainsrocks.ru/",
+  "http://movies.balineseleaf.nomoredomainsrocks.ru/",
+  "http://localhost:3000",
+  "http://localhost:3001",
 ];
 
 module.exports = (req, res, next) => {
   const { method } = req; // тип запроса (HTTP-метод)
   const { origin } = req.headers; // источники запросов
   // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
   }
   // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
+  if (method === "OPTIONS") {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
