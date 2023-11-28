55a69bc6bed29a9377b909a9494d88dc8f03113d
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-11-28 18:32:05 +0300

    add semi quotes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,29 +5,31 @@ const { NODE_ENV, JWT } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 function auth(req, res, next) {
-  const { authorization } = req.cookies;
+  const { authorization } = req.headers;
+  console.log('1', authorization);
+  console.log('2', req.headers);
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    // Сначала обработаем ошибку — случай, когда токена нет в заголовке:
+  if (!authorization || !authorization.startsWith('Bearer ')) { // Сначала обработаем ошибку — случай, когда токена нет в заголовке:
     return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', ''); // извлекаем токен из заголовка и выкидываем приставку Bearer, таким образом, в переменную token запишется только JWT.
+  console.log('token', token);
   let payload;
 
   try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === 'production' ? JWT : 'dev-secret',
-      { expiresIn: '7d' },
-    ); // После извле ток из запроса нужно убед,
-  } catch (err) {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT : 'dev-secret', { expiresIn: '7d' }); // После извле ток из запроса нужно убед,
+  } catch (err) { // что польз прислал тот токен, кот был выдан ему ранее.
+    // Такую проверку осуществляет метод verify модуля jsonwebtoken. Метод принимает на вход два
+    // параметра —
+    // токен и секретный ключ, которым этот токен был подписан:
+    // Метод jwt.verify вернёт пейлоуд токена
     return next(new UnauthorizedError('Некорректный токен'));
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
+  req.user = payload;// записываем пейлоуд в объект запроса
 
-  return next(); // пропускаем запрос дальше
+  return next();// пропускаем запрос дальше
 }
 
 module.exports = auth;
